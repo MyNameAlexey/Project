ca4e8255d615429408696f5eb3cfa90670a1d89b
Alexey Ponomarev <denver0-1986@mail.ru>
2023-04-02 17:22:58 +0300

    commit3 output of the result

--- project/Program.cs
+++ project/Program.cs
@@ -7,7 +7,7 @@ string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
-    for (int i = 0; i < i < array1.Length; i++)
+    for (int i = 0; i < array1.Length; i++)
     {
         if(array1[i].Length <= 3)
         {
@@ -16,3 +16,13 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
